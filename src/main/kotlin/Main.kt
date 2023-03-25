42faead78c7431f898828646d5920e01be6ca134
changja88 <changja00@gmail.com>
2023-03-26 00:44:19 +0900

    기타 수정

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,24 +2,4 @@ import java.lang.System
 import java.util.Scanner
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-}
-
-
-class Caculater2 constructor(initialNum: Int = 0) {
-    var result: Int = 10
-        set(value) {
-            field = value
-        }
-
-    init {
-        this.result = initialNum
-    }
-    fun calculater(operater:Char, num: Int){
-
-    }
 }
